43eb4ce09f279f66e63e85050470329aee14cb2f
Shad-dock <strangepear@mail.ru>
2026-03-04 23:10:24 +0300

    Add files via upload

---
+++ testGit/src/main/java/com/mycompany/testgit/TestGit.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.testgit;
+
+/**
+ *
+ * @author LEGION
+ */
+public class TestGit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+    }
+    
+}
