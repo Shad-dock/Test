9cf1a00c547c8a4ae346370034b6bb812f21b514
Anton <strangepear@mail.ru>
2026-03-04 23:14:39 +0300

    com1

--- testGit/src/main/java/com/mycompany/testgit/TestGit.java
+++ testGit/src/main/java/com/mycompany/testgit/TestGit.java
@@ -12,7 +12,7 @@ public class TestGit {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        
+        System.out.println("testsddwsadq");
     }
     
 }
